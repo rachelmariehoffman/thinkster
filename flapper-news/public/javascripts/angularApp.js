1f7129e50d586877d96360a410411c796026855c
rhoffmanssf <rhoffman@softstackfactory.org>
2016-02-22 23:04:27 +0000

    commit all files

---
+++ flapper-news/public/javascripts/angularApp.js
@@ -0,0 +1,64 @@
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state ('home', {
+      url: '/home',
+      templateUrl:'/home.html',
+      controller: 'MainCtrl'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    });
+  $urlRouterProvider.otherwise('home');
+}]);
+
+app.factory('posts', [function() {
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
+
+app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
+  $scope.test = 'Hello world!';
+  $scope.posts = posts.posts;
+  $scope.addPost = function() {
+    if (!$scope.title || $scope.title === "") { return; } 
+    $scope.posts.push({
+      title: $scope.title, 
+      link: $scope.link,
+      upvotes: 0,
+      comments: []
+    });
+    $scope.title = "";
+    $scope.link = "";
+  };
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes ++;
+  };
+  $scope.decrementUpvotes = function(post) {
+    post.upvotes --;
+  };
+}]);
+
+app.controller('PostsCtrl', ['$scope', '$stateParams', 'posts', function($scope, $stateParams, posts){
+  $scope.post = posts.posts[$stateParams.id];
+  $scope.addComment = function(){
+    if($scope.body === '') { return; }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
+  };
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes ++;
+  };
+  $scope.decrementUpvotes = function(post) {
+    post.upvotes --;
+  };
+}]);
